ad60bff63308a5397a32824e2c07aab105a9c308
gauss963 <gauss963@gmail.com>
2024-10-14 13:08:14 +0800

    Finish

--- code/GPSLocating.f90
+++ code/GPSLocating.f90
@@ -69,17 +69,17 @@ program GPSLocating
     contains
 
     subroutine solve_3x3(A, B, X)
-    real(dp), intent(in) :: A(3,3), B(3)
+    real(dp), intent(in) :: A(3, 3), B(3)
     real(dp), intent(out) :: X(3)
     real(dp) :: detA, detA1, detA2, detA3
-    real(dp), dimension(3,3) :: A1, A2, A3
+    real(dp), dimension(3, 3) :: A1, A2, A3
 
     ! Compute determinant of A
     detA = determinant_3x3(A)
     if (abs(detA) < 1.0e-12_dp) then
         print *, 'Matrix is singular or ill-conditioned.'
         stop
-    endif
+    end if
 
     ! Replace columns of A with B to compute determinants
     A1 = A
