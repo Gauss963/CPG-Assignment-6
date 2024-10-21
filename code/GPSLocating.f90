2adca946ba36acdc5343acfbd4b28a1fd0403d09
gauss963 <gauss963@gmail.com>
2024-10-21 18:42:58 +0800

    Finish GPS in the correct way.

--- code/GPSLocating.f90
+++ code/GPSLocating.f90
@@ -3,15 +3,12 @@ program GPSLocating
     implicit none
 
     integer, parameter :: dp = real64
-    integer :: N, i, j
-    real(dp), allocatable :: xi(:), yi(:), zi(:), Di(:)
-    real(dp), allocatable :: A(:,:), L(:), At(:,:), AtA(:,:), AtL(:)
-    real(dp), allocatable :: X(:), solution(:)
-    real(dp) :: D1, x1, y1, z1
-    real(dp) :: det
-    
-    ! For LAPACK routines
-    ! integer :: info, nrhs, lda, ldb
+    integer :: N, i, j, k
+    real(dp), allocatable :: v_QC1(:), v_QC2(:), v_QC3(:), v_d(:), v_m(:)
+    real(dp), allocatable :: m_Q(:,:), m_QT(:,:)
+
+    ! For LAPACK routine DGELS
+    external :: DGELS
 
     ! Prompt user for the number of stations
     print *, 'Enter the number of stations (N >= 4):'
@@ -22,50 +19,41 @@ program GPSLocating
     end if
 
     ! Allocate arrays
-    allocate(xi(N), yi(N), zi(N), Di(N))
-    allocate(A(N-1, 3), L(N-1))
-    allocate(At(3, N-1), AtA(3, 3), AtL(3))
-    allocate(X(3), solution(3))
+    allocate(v_QC1(N), v_QC2(N), v_QC3(N), v_d(N), v_m(N))
 
-    ! Input station coordinates and distances
-    print *, 'Enter the coordinates (xi, yi, zi) and distance Di for each station:'
+    ! Input station coorv_dnates and v_dstances
+    print *, 'Enter the coorv_dnates (v_QC1, v_QC2, v_QC3) and v_dstance v_d for each station:'
     do i = 1, N
         print *, 'Station ', i, ':'
-        read *, xi(i), yi(i), zi(i), Di(i)
+        read *, v_QC1(i), v_QC2(i), v_QC3(i), v_d(i)
     end do
 
-    ! Use the first station as the reference
-    x1 = xi(1)
-    y1 = yi(1)
-    z1 = zi(1)
-    D1 = Di(1)
-
-    ! Construct matrix A and vector L
-    do i = 2, N
-        A(i-1, 1) = -2.0_dp * (xi(i) - x1)
-        A(i-1, 2) = -2.0_dp * (yi(i) - y1)
-        A(i-1, 3) = -2.0_dp * (zi(i) - z1)
-        L(i-1) = (Di(i)**2 - D1**2) - (xi(i)**2 + yi(i)**2 + zi(i)**2) + (x1**2 + y1**2 + z1**2)
+    allocate(m_Q(N, 3), m_QT(3, N))
+    do j = 1, N
+        m_Q(j, 1) = v_QC1(j)
+        m_Q(j, 2) = v_QC2(j)
+        m_Q(j, 3) = v_QC3(j)
     end do
 
-    ! Transpose of A
-    At = transpose(A)
+    ! Print the matrix to check
+    ! print *, 'Matrix Q:'
+    ! do k = 1, N
+    !     print *, m_Q(k, 1), m_Q(k, 2), m_Q(k, 3)
+    ! end do
 
-    ! Compute AtA and AtL
-    AtA = matmul(At, A)
-    AtL = matmul(At, L)
+    m_QT = transpose(m_Q)
+    v_m = matmul(matmul(matmul(m_QT, m_q)**(-1), m_QT), v_d) ! Complete this part
 
-    ! Solve the normal equations AtA * X = AtL
-    ! For simplicity, we'll use Cramer's rule for 3x3 system
-    call solve_3x3(AtA, AtL, solution)
-
-    ! The coordinates of the target point
-    X = solution
 
     ! Output the result
     print *, 'The estimated coordinates of the target point are:'
-    print *, 'X = ', X(1)
-    print *, 'Y = ', X(2)
-    print *, 'Z = ', X(3)
+    print *, 'X = ', v_m(1)
+    print *, 'Y = ', v_m(2)
+    print *, 'Z = ', v_m(3)
+
+
+
+    deallocate(v_QC1, v_QC2, v_QC3, v_d, v_m)
+    deallocate(m_Q)
 
 end program GPSLocating
